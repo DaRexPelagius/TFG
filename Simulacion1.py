dc211e13e279af709943d1ee57fcc3cde0f37650
DaRexPelagius <jluis.suarezfueyo@gmail.com>
2018-06-18 09:49:18 +0200

    Hola

--- Simulacion1.py
+++ Simulacion1.py
@@ -15,48 +15,56 @@ class Simulacion1(object):
         100 unidades de tiempo para tamanyos desde 100 a 1000000."""
 
         tam = [100, 1000, 5000, 10000, 25000, 50000, 100000, 1000000]
+        tiempo = 100
+        beta = 0.025
+        gamma = 0.01
         resultados = []
 
         for s in tam:
-            print s
+            print "Simulacion 1 Tamanyo: %d" % s
             if s != 1000000:
-                aux = FuncionesNucleo().simulacion(ModeloSIR, "Scale_Free",
-                                                   tam_grafo=s, time=300,
-                                                   beta=0.025, gamma=0.01)
+                aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR,
+                                                         "Scale_Free",
+                                                         tam_grafo=s,
+                                                         time=tiempo,
+                                                         beta=beta, gamma=gamma)
 
                 ## CdE
-                media = [mean(items) for items in izip(*aux)]
-                print media
-                while abs(mean(media)) - 0.1 < 0:
-                    aux = FuncionesNucleo().simulacion(ModeloSIR, "Scale_Free",
-                                                       tam_grafo=s, time=300,
-                                                       beta=0.025, gamma=0.01)
-                    media = [mean(items) for items in izip(*aux)]
+                media = [mean(items) for items in izip(*aux2)]
+                while abs(mean(media)) - 0.05 < 0:
+                    aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR,
+                                                             "Scale_Free",
+                                                             tam_grafo=s,
+                                                             time=tiempo,
+                                                             beta=beta,
+                                                             gamma=gamma)
+                    media = [mean(items) for items in izip(*aux2)]
                 # Anyadimos el resultado
                 resultados.append(aux)
 
                 # La simulacion con un millon de nodos solo la haremos una vez
                 # por limitaciones del HW.
             else:
-                aux = FuncionesNucleo().simulacion(ModeloSIR, "Scale_Free",
-                                                   tam_grafo=s, runs=1,
-                                                   time=300, beta=0.025,
-                                                   gamma=0.01)
+                aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR,
+                                                         "Scale_Free",
+                                                         tam_grafo=s, runs=1,
+                                                         time=tiempo, beta=beta,
+                                                         gamma=gamma)
 
                 ## CdE
-                media = [mean(items) for items in izip(*aux)]
-                while abs(mean(media)) - 0.1 < 0:
-                    aux = FuncionesNucleo().simulacion(ModeloSIR,
-                                                       "Scale_Free",
-                                                       tam_grafo=s,
-                                                       time=300,
-                                                       beta=0.025,
-                                                       gamma=0.01)
-                    media = [mean(items) for items in izip(*aux)]
+                media = [mean(items) for items in izip(*aux2)]
+                while abs(mean(media)) - 0.05 < 0:
+                    aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR,
+                                                             "Scale_Free",
+                                                             tam_grafo=s,
+                                                             time=tiempo,
+                                                             beta=beta,
+                                                             gamma=gamma)
+                    media = [mean(items) for items in izip(*aux2)]
                 # Anyadimos el resultado
                 resultados.append(aux)
 
-        tiempo = range(300)
+        tiempo = range(tiempo)
         averaged = [mean(items) for items in izip(*resultados[0])]
         averaged2 = [mean(items) for items in izip(*resultados[1])]
         averaged3 = [mean(items) for items in izip(*resultados[2])]
@@ -76,7 +84,8 @@ class Simulacion1(object):
         plt.plot(tiempo, averaged8, 'brown', label='n = 10000000')
         plt.legend(title="Numero de nodos del grafo")
         plt.title("Proporcion de infectados a lo largo de 100u de tiempo.")
-        plt.savefig('Figuras/simulacion1.png')
+        plt.show()
+        #plt.savefig('Figuras/simulacion1.png')
 
 
 ejec = Simulacion1()
--- Simulacion2.py
+++ Simulacion2.py
@@ -10,37 +10,61 @@ class Simulacion2(object):
 
     def main(self):
         """ En esta segunda simulación vamos a comprobar como afecta
-         en una epidemia si el virus empieza en un hub o no. """
+         en una epidemia si el virus empieza en un hub o no. Para ello
+         utilizaremos los mismos parámetros que en el apartado anterior
+         a la hora de realizar la simulacion, salvo a la hora de escoger el
+         primer nodo infectado."""
 
         tam = 100000
+        tiempo = 100
+        beta = 0.025
+        gamma = 0.01
         resultados = []
 
         print "Simulacion 1"
-        resultados.append(FuncionesNucleo().simulacionHub(ModeloSIR,
-                                                          "Scale_Free",
-                                                          tam_grafo=tam,
-                                                          susceptibles=tam,
-                                                          time=300, beta=0.025,
-                                                          gamma=0.01))
-        print "Simulacion 2"
-        resultados.append(
-            FuncionesNucleo().simulacionAntiHub(ModeloSIR, "Scale_Free",
-                                                tam_grafo=tam,
-                                                susceptibles=tam,
-                                                time=300, beta=0.025,
-                                                gamma=0.01))
+        aux, aux2 = FuncionesNucleo().simulacionHub(ModeloSIR,
+                                                    "Scale_Free",
+                                                    tam_grafo=tam,
+                                                    time=tiempo, beta=beta,
+                                                    gamma=gamma)
+        ## CdE
+        media = [mean(items) for items in izip(*aux2)]
+        while abs(mean(media)) - 0.05 < 0:
+            aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR, "Scale_Free",
+                                                     tam_grafo=tam, time=tiempo,
+                                                     beta=beta, gamma=gamma)
+            media = [mean(items) for items in izip(*aux2)]
 
-        tiempo = range(300)
+        resultados.append(aux)
+
+        print "Simulacion 2"
+        aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR, "Scale_Free",
+                                                 tam_grafo=tam,
+                                                 time=tiempo, beta=beta,
+                                                 gamma=gamma)
+        ## CdE
+        media = [mean(items) for items in izip(*aux2)]
+        while abs(mean(media)) - 0.05 < 0:
+            aux, aux2 = FuncionesNucleo().simulacion(ModeloSIR, "Scale_Free",
+                                                     tam_grafo=tam, time=tiempo,
+                                                     beta=beta, gamma=gamma)
+            media = [mean(items) for items in izip(*aux2)]
+
+        resultados.append(aux)
+
+        tiempo = range(tiempo)
         averaged = [mean(items) for items in izip(*resultados[0])]
         averaged2 = [mean(items) for items in izip(*resultados[1])]
 
         plt.plot(tiempo, averaged, 'r',
                  label='Infeccion iniciada en un hub')
         plt.plot(tiempo, averaged2, 'b',
-                 label='Infeccion no iniciada en un hub')
+                 label='Infeccion iniciada de forma aleatoria')
         plt.legend(title="Hubs vs nodos normales")
         plt.title("Proporcion de infectados a lo largo de 100u de tiempo.")
-        plt.savefig('Figuras/simulacion2.png')
+        # Plot
+        plt.show()
+        #plt.savefig('Figuras/simulacion2.png')
 
 
 ejec = Simulacion2()
